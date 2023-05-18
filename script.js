a3a12e2e86c16956c3891784f1bcc51abcb44692
Filip Nyman <filnym@hotmail.com>
2023-05-18 14:46:59 +0200

    test av olika properties choices library

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 import Choices from "choices.js";
 // import 'choices.js'
-import 'choices.js/public/assets/styles/choices.css';
+import "choices.js/public/assets/styles/choices.css";
 
-document.addEventListener("DOMContentLoaded", function () {
-    var selectElement = document.getElementById("kategorier-select");
-    new Choices(selectElement);
-  });
+// document.addEventListener("DOMContentLoaded", function () {
+//     var selectElement = document.getElementById("kategorier-select");
+//     new Choices(selectElement);
+//   });
+
+const myDropdown = new Choices(kategorier - select, {
+  shouldSort: false, // Inaktivera standardsorteringen
+  callbackOnCreateTemplates: function (template) {
+    return {
+      choice: (classNames, data) => {
+        return template(`
+          <div class="${classNames.item} ${classNames.itemSelectable}" data-item data-id="${data.id}" data-value="${data.value}" style="width: auto;">
+            ${data.label}
+          </div>
+        `);
+      },
+    };
+  },
+});
