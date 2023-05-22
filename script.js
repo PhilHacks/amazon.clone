edbb42e3f117bfdf4c977a0e4ce3d6ab3b4f59a6
Filip Nyman <filnym@hotmail.com>
2023-05-22 21:22:37 +0200

    La script src i body istället för head

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-import Choices from "choices.js";
-// import 'choices.js'
-import "choices.js/public/assets/styles/choices.css";
+// import Choices from "choices.js";
+// // import 'choices.js'
+// import "choices.js/public/assets/styles/choices.css";
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     var selectElement = document.getElementById("kategorier-select");
-//     new Choices(selectElement);
-//   });
+// // document.addEventListener("DOMContentLoaded", function () {
+// //     var selectElement = document.getElementById("kategorier-select");
+// //     new Choices(selectElement);
+// //   });
 
-const myDropdown = new Choices(kategorier - select, {
-  shouldSort: false, // Inaktivera standardsorteringen
-  callbackOnCreateTemplates: function (template) {
-    return {
-      choice: (classNames, data) => {
-        return template(`
-          <div class="${classNames.item} ${classNames.itemSelectable}" data-item data-id="${data.id}" data-value="${data.value}" style="width: auto;">
-            ${data.label}
-          </div>
-        `);
-      },
-    };
-  },
-});
+// const myDropdown = new Choices(kategorier - select, {
+//   shouldSort: false, // Inaktivera standardsorteringen
+//   callbackOnCreateTemplates: function (template) {
+//     return {
+//       choice: (classNames, data) => {
+//         return template(`
+//           <div class="${classNames.item} ${classNames.itemSelectable}" data-item data-id="${data.id}" data-value="${data.value}" style="width: auto;">
+//             ${data.label}
+//           </div>
+//         `);
+//       },
+//     };
+//   },
+// });
+
+const selectElement = document.getElementById("kategorier-select");
+
+const updateDropdownWidth = () => {
+  const selectedOption = selectElement.options[selectElement.selectedIndex];
+  const textWidth = selectedOption.text.length * 10; // Justera konstanten för att passa din design
+  selectElement.style.width = textWidth + "px";
+};
+
+selectElement.addEventListener("change", updateDropdownWidth);
+window.addEventListener("load", updateDropdownWidth);
