75ca95144fec2b61441d526f4898096f325ba48a
Filip Nyman <filnym@hotmail.com>
2023-06-15 11:01:29 +0200

    implementation of robin help

--- script.js
+++ script.js
@@ -1,53 +1,16 @@
-// import Choices from "choices.js";
-// // import 'choices.js'
-// import "choices.js/public/assets/styles/choices.css";
+const buttonLabel = document.querySelector(".button-label");
+const selectElement = document.querySelector(".select");
 
-// // document.addEventListener("DOMContentLoaded", function () {
-// //     var selectElement = document.getElementById("kategorier-select");
-// //     new Choices(selectElement);
-// //   });
+// När sidan laddas körs den här. Har för enkelhetens skull hårdkodat första valet.
+// Detta sätts alltså i den snygga "knappens" label-span. textContent här är ett
+// säkrare alternativ till innerHTML som också kan användas för att byta innehåll.
+// textContent slägger dock bara igenom text och är svårara att "hacka" med fult innehåll.
+buttonLabel.textContent = "Alla kategorier...";
 
-// const myDropdown = new Choices(kategorier - select, {
-//   shouldSort: false, // Inaktivera standardsorteringen
-//   callbackOnCreateTemplates: function (template) {
-//     return {
-//       choice: (classNames, data) => {
-//         return template(`
-//           <div class="${classNames.item} ${classNames.itemSelectable}" data-item data-id="${data.id}" data-value="${data.value}" style="width: auto;">
-//             ${data.label}
-//           </div>
-//         `);
-//       },
-//     };
-//   },
-// });
-
-//Ny implemention med vanilla.js
-const selectElement = document.getElementById("kategorier-select");
-
-const updateDropdownWidth = (selectElement) => {
-  const selectedOption = selectElement.options[selectElement.selectedIndex];
-  const selectedText = selectedOption.textContent.trim();
-
-  const tempSpan = document.createElement("span");
-  // tempSpan.style.visibility = 'hidden';
-  // tempSpan.style.whiteSpace = 'nowrap';
-  tempSpan.textContent = selectedText;
-  document.body.appendChild(tempSpan);
-  console.log("AppendChild");
-  console.log("tempSpan", tempSpan);
-
-  const textWidth = tempSpan.clientWidth;
-  document.body.removeChild(tempSpan);
-  // const textWidth = selectedOption.clientWidth; // selectedOption.text.length * 10; // Justera konstanten för att passa din design
-  console.log("Bredd", textWidth);
-  selectElement.style.width = textWidth + "px";
-};
-
-selectElement.addEventListener("change", () =>
-  updateDropdownWidth(selectElement)
-);
-document.addEventListener("DOMContentLoaded", () =>
-  updateDropdownWidth(selectElement)
-);
-// window.addEventListener("load", updateDropdownWidth);
+// En eventlyssnare sätts här på select-elementet och lyssnar på förändringar (change).
+// När selecten byter värde så triggas funktionen där som tar in ett sk. event för ändringen.
+// Eventet innehåller massa information, men bl.a. det nya värdet (event.target.value) för select
+// som vi använder för att sätta ett nytt textContent på button labeln.
+selectElement.addEventListener("change", (event) => {
+  buttonLabel.textContent = event.target.value;
+});
