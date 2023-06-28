7d1d401e1be3abc34b043f8b767eea1d1460de0b
Benjamin Berg <berg.benjamin.z@gmail.com>
2023-06-28 13:56:21 +0200

    knappen är klar!

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const selectElement = document.querySelector(".select");
 // Detta sätts alltså i den snygga "knappens" label-span. textContent här är ett
 // säkrare alternativ till innerHTML som också kan användas för att byta innehåll.
 // textContent slägger dock bara igenom text och är svårara att "hacka" med fult innehåll.
-buttonLabel.textContent = "Alla kategorier...";
+// buttonLabel.textContent = "Alla kategorier...";
+
+buttonLabel.textContent = selectElement.options[0].textContent + ' ⯆';
 
 // En eventlyssnare sätts här på select-elementet och lyssnar på förändringar (change).
 // När selecten byter värde så triggas funktionen där som tar in ett sk. event för ändringen.
@@ -15,7 +17,7 @@ selectElement.addEventListener("change", (event) => {
   const selectedIndex = event.target.selectedIndex;
   const selectedOption = event.target.options[selectedIndex];
   const optionText = selectedOption.textContent;
-  buttonLabel.textContent = optionText;
+  buttonLabel.textContent = optionText + ' ⯆';
 });
 
 //rad att hämtar från select (value)
