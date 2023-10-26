dfe314233777106d9d8899112fa445487d0ee98c
Phil <filip.nyman7@gmail.com>
2023-10-26 13:01:41 +0200

    refactor code with small func and update names

--- script.js
+++ script.js
@@ -43,25 +43,32 @@ const closeMenu = (event) => {
   }
 };
 
+// Ensures event listeners are initialized after document load.
 document.addEventListener("DOMContentLoaded", initializeSideMenuEvents);
 
 // ----------- Hidden menu toggle ------------
 const showButton = document.getElementById("show");
-const hideMenu = document.getElementById("hidden-menu");
+const hiddenMenuElement = document.getElementById("hidden-menu");
 let hiddenMenuShown = false;
 
-showButton.addEventListener("click", (event) => {
-  event.stopPropagation();
-  if (hiddenMenuShown) {
-    hideMenu.style.display = "none";
-    showButton.textContent = "Visa alla";
-    hiddenMenuShown = false;
-  } else {
-    hideMenu.style.display = "flex";
-    showButton.textContent = "Visa färre";
-    hiddenMenuShown = true;
-  }
-});
+const initializeShowButton = () => {
+  showButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    return hiddenMenuShown ? hideToggle() : showToggle();
+  });
+};
+
+const showMenu = () => {
+  hiddenMenuElement.style.display = "none";
+  showButton.textContent = "Visa alla";
+  hiddenMenuShown = false;
+};
+
+const hideMenu = () => {
+  hiddenMenuElement.style.display = "flex";
+  showButton.textContent = "Visa färre";
+  hiddenMenuShown = true;
+};
 
 // ----------- scrollToTop ------------
 const scrollToTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
